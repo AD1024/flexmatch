e8b5ab6a22b26326af38525274f88ddfdc1d74cb
AD1024 <dh63@cs.washington.edu>
2021-09-10 06:27:14 -0700

    [ add ] model

--- tests/test_recexpr_compiler.py
+++ tests/test_recexpr_compiler.py
@@ -1,9 +1,38 @@
 import json
 import tvm
+import torch
+from tvm.contrib import graph_executor
+from resmlp import ResMLP
 from tvm import relay
 from megraph.language import RecExprCompiler
 
-def test_compile(filename):
+def run_baseline(input_data):
+    model = ResMLP(
+        image_size = 32,
+        patch_size = 16,
+        dim = 64,
+        depth = 3,
+        num_classes = 32
+    )
+    model.eval()
+    with torch.no_grad():
+        baseline_outputs = model(*[input.clone() for input in input_data])
+
+    if isinstance(baseline_outputs, tuple):
+        baseline_outputs = tuple(out.cpu().numpy() for out in baseline_outputs)
+    else:
+        baseline_outputs = (baseline_outputs.cpu().numpy(),)
+
+    return baseline_outputs
+
+def run_relay(mod, input):
+    compiled_input = [inp.clone().cpu().numpy() for inp in input]
+    target = tvm.target.create('llvm')
+    vm = relay.create_executor('vm', target=target, mod=mod)
+    executor = vm.evaluate()
+    return executor(*compiled_input).asnumpy()
+
+def test_compile(filename, print_model=True):
     with open(filename, 'r') as fp:
         recexpr_json = json.load(fp)
         compiler = RecExprCompiler({
@@ -11,24 +40,7 @@ def test_compile(filename):
         }, {
             'flex-linear': 'ilaflex.linear'
         })
-        # %input0: Tensor[(1, 3, 32, 32), float32]
-        # %v1_weight: Tensor[(64, 768), float32]
-        # %v1_bias: Tensor[(64), float32]
-        # %v2_0_affine_g: Tensor[(1, 1, 64), float32]
-        # %v2_0_affine_b: Tensor[(1, 1, 64), float32]
-        # %v2_0_fn_weight: Tensor[(4, 4, 1), float32]
-        # %v2_0_fn_bias: Tensor[(4), float32]
-        # %v2_0_scale: Tensor[(1, 1, 64), float32]
-        # %v2_1_affine_g: Tensor[(1, 1, 64), float32]
-        # %v2_1_affine_b: Tensor[(1, 1, 64), float32]
-        # %v2_1_fn_0_weight: Tensor[(256, 64), float32]
-        # %v2_1_fn_0_bias: Tensor[(256), float32]
-        # %v2_1_fn_2_weight: Tensor[(64, 256), float32]
-        # %v2_1_fn_2_bias: Tensor[(64), float32]
-        # %v2_1_scale: Tensor[(1, 1, 64), float32]
-        # %v3_g: Tensor[(1, 1, 64), float32]
-        # %v3_b: Tensor[(1, 1, 64), float32]
-        # %v5_weight: Tensor[(32, 64), float32], %v5_bias: Tensor[(32), float32]
+ 
         expr = compiler.to_relay_expr(recexpr_json, {
             'input0': (1, 3, 32, 32),
             'v1_weight': (64, 768),
@@ -45,14 +57,51 @@ def test_compile(filename):
             'v2_1_fn_2_weight': (64, 256),
             'v2_1_fn_2_bias': (64,),
             'v2_1_scale': (1, 1, 64),
-            'v3_g': (1, 1, 64),
-            'v3_b': (1, 1, 64),
-            'v5_weight': (32, 64),
-            'v5_bias': (32,)
+            'v3_0_affine_g': (1, 1, 64),
+            'v3_0_affine_b': (1, 1, 64),
+            'v3_0_fn_weight': (4, 4, 1),
+            'v3_0_fn_bias': (4,),
+            'v3_0_scale': (1, 1, 64),
+            'v3_1_affine_g': (1, 1, 64),
+            'v3_1_affine_b': (1, 1, 64),
+            'v3_1_fn_0_weight': (256, 64),
+            'v3_1_fn_0_bias': (256,),
+            'v3_1_fn_2_weight': (64, 256),
+            'v3_1_fn_2_bias': (64,),
+            'v3_1_scale': (1, 1, 64),
+            'v4_0_affine_g': (1, 1, 64),
+            'v4_0_affine_b': (1, 1, 64),
+            'v4_0_fn_weight': (4, 4, 1),
+            'v4_0_fn_bias': (4,),
+            'v4_0_scale': (1, 1, 64),
+            'v4_1_affine_g': (1, 1, 64),
+            'v4_1_affine_b': (1, 1, 64),
+            'v4_1_fn_0_weight': (256, 64),
+            'v4_1_fn_0_bias': (256,),
+            'v4_1_fn_2_weight': (64, 256),
+            'v4_1_fn_2_bias': (64,),
+            'v4_1_scale': (1, 1, 64),
+            'v5_g': (1, 1, 64),
+            'v5_b': (1, 1, 64),
+            'v7_weight': (32, 64),
+            'v7_bias': (32,),
         })
         mod = tvm.ir.IRModule.from_expr(expr)
-        print(mod)
-        print(relay.transform.InferType()(mod))
+        mod = relay.transform.InferType()(mod)
+        if print_model:
+            print(mod)
+
+        input = torch.randn(1, 3, 32, 32)
+        baseline_output = run_baseline([input])
+        relay_output = run_relay(mod, [input])
+
+        print(baseline_output)
+        print('--------------------')
+        print(relay_output)
+
+        # tvm.testing.assert_allclose(
+        #     baseline_output, compiled_output, rtol=rtol, atol=atol
+        # )
 
 if __name__ == '__main__':
-    test_compile('tests/resmlp-dump.json')
+    test_compile('resmlp-dump.json')
--- tests/test_resmlp.py
+++ tests/test_resmlp.py
@@ -1,5 +1,6 @@
 # Res-MLP implementation taken from https://github.com/lucidrains/res-mlp-pytorch/blob/7a5b5276cd9270ad8131f77dfe4e6f56fe65fb3f/res_mlp_pytorch/res_mlp_pytorch.py
 from megraph.matcher import check_and_annotate
+from .resmlp import ResMLP
 import torch
 import tvm
 import megraph
@@ -8,68 +9,8 @@ import tvm.testing
 from tvm.contrib import graph_executor
 from tvm.relay.op.contrib import ilavta
 from tvm.relay.op.contrib import ilaflex
-from torch import nn, einsum
-from einops.layers.torch import Rearrange, Reduce
 from tvm.relay import ExprMutator
 
-
-class Affine(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.g = nn.Parameter(torch.ones(1, 1, dim))
-        self.b = nn.Parameter(torch.zeros(1, 1, dim))
-
-    def forward(self, x):
-        return x * self.g + self.b
-
-
-class PreAffinePostLayerScale(nn.Module):  # https://arxiv.org/abs/2103.17239
-    def __init__(self, dim, depth, fn):
-        super().__init__()
-        if depth <= 18:
-            init_eps = 0.1
-        elif depth > 18 and depth <= 24:
-            init_eps = 1e-5
-        else:
-            init_eps = 1e-6
-
-        scale = torch.zeros(1, 1, dim).fill_(init_eps)
-        self.scale = nn.Parameter(scale)
-        self.affine = Affine(dim)
-        self.fn = fn
-
-    def forward(self, x):
-        return self.fn(self.affine(x)) * self.scale + x
-
-
-def ResMLP(*, image_size, patch_size, dim, depth, num_classes, expansion_factor=4):
-    assert (image_size % patch_size) == 0, "image must be divisible by patch size"
-    num_patches = (image_size // patch_size) ** 2
-    wrapper = lambda i, fn: PreAffinePostLayerScale(dim, i + 1, fn)
-
-    return nn.Sequential(
-        Rearrange(
-            "b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=patch_size, p2=patch_size
-        ),
-        nn.Linear((patch_size ** 2) * 3, dim),
-        *[
-            nn.Sequential(
-                wrapper(i, nn.Conv1d(num_patches, num_patches, 1)),
-                wrapper(
-                    i,
-                    nn.Sequential(
-                        nn.Linear(dim, dim * expansion_factor),
-                        nn.GELU(),
-                        nn.Linear(dim * expansion_factor, dim),
-                    ),
-                ),
-            )
-            for i in range(depth)
-        ],
-        Affine(dim),
-        Reduce("b n c -> b c", "mean"),
-        nn.Linear(dim, num_classes)
-    )
 
 def assert_shapes_match(tru, est):
     if tru.shape != est.shape:
@@ -149,13 +90,13 @@ def verify_model(
     )
     egraph = megraph.load_egraph('tests/linear_pattern.egraph')
     # Dump the model with renamed variables (seems rust frontend cannot parse variables with dots in their names)
-    mutator = RenameMutator()
-    new_body = mutator.visit(mod["main"].body)
-    new_mod = tvm.ir.IRModule.from_expr(relay.Function(relay.analysis.free_vars(new_body), new_body))
-    new_mod = relay.transform.InferType()(new_mod)
-    with open('resmlp.relay', 'w') as fp:
-        fp.write(new_mod.astext())
-    return
+    # mutator = RenameMutator()
+    # new_body = mutator.visit(mod["main"].body)
+    # new_mod = tvm.ir.IRModule.from_expr(relay.Function(relay.analysis.free_vars(new_body), new_body))
+    # new_mod = relay.transform.InferType()(new_mod)
+    # with open('resmlp.relay', 'w') as fp:
+    #     fp.write(new_mod.astext())
+    # return
     mut_main = check_and_annotate(mod["main"].body, (egraph, 'ilaflex', 'ilaflex.linear'))
     mod = tvm.ir.IRModule.from_expr(mut_main)
     if print_model:
