2f38b6d0782aea20b4a314b9829aaa4c22ac314a
AD1024 <dh63@cs.washington.edu>
2021-09-16 02:59:31 +0000

    [ update ] test

--- tests/test_recexpr_compiler.py
+++ tests/test_recexpr_compiler.py
@@ -2,9 +2,12 @@ import json
 import tvm
 import tvm.testing
 import torch
+import time
+import random
 from tvm.contrib import graph_executor
 from resmlp import ResMLP
 from tvm import relay
+from tvm.relay.op.contrib import ilaflex
 from megraph.language import RecExprCompiler
 
 def run_baseline(model, input_data):
@@ -19,7 +22,16 @@ def run_baseline(model, input_data):
 
     return baseline_outputs
 
-def test_compile(filename, relay_src, print_model=True):
+def run_passes(mod):
+    patterns = ilaflex.pattern_table()
+    mod = tvm.relay.transform.MergeComposite(patterns)(mod)
+    mod = tvm.relay.transform.AnnotateTarget('ilaflex')(mod)
+    mod = tvm.relay.transform.PartitionGraph()(mod)
+    print('[Python] Transformation complete')
+    mod = relay.transform.InferType()(mod)
+    return mod
+
+def test_compile(filename, relay_src, print_model=True, depth=1):
     relay_code = open(relay_src, 'r').read()
     with open(filename, 'r') as fp:
         recexpr_json = json.load(fp)
@@ -33,19 +45,25 @@ def test_compile(filename, relay_src, print_model=True):
         shape_dict = dict()
         for args in original_model['main'].params:
             shape_dict[args.name_hint] = tuple(args.type_annotation.shape)
- 
+
+        start = time.time()
         expr = compiler.to_relay_expr(recexpr_json, shape_dict)
         mod = tvm.ir.IRModule.from_expr(expr)
         mod = relay.transform.InferType()(mod)
+        end = time.time()
         baseline = ResMLP(
             image_size = 32,
             patch_size = 16,
             dim = 64,
-            depth = 1,
+            depth = depth,
             num_classes = 32
         )
         if print_model:
-            print(mod)
+            with open('resmlp-compiled.relay', 'w') as fp:
+                fp.write(str(mod))
+                print(f'[DEBUG] Time to compile from RecExpr: {(end - start) * 1000} ms')
+                print(f'[INFO] Compiled model saved to resmlp-compiled.relay')
+            return
 
         img_input = [torch.randn(1, 3, 32, 32)]
         baseline_outputs = run_baseline(baseline, img_input)
@@ -58,12 +76,12 @@ def test_compile(filename, relay_src, print_model=True):
         for k, v in params.items():
             n_params[f'v{k.replace(".", "_")}'] = v
         compiled_input = dict(zip(input_names, [inp.clone().cpu().numpy() for inp in img_input]))
- 
-        print(f'baseline: {baseline_outputs}')
         with tvm.transform.PassContext(opt_level=0):
             for target, dev in tvm.testing.enabled_targets():
-                print(f'target: {target} | device: {dev}')
+                # print(f'target: {target} | device: {dev}')
+                start = time.time()
                 relay_graph, lib, params = relay.build(mod, target=target, params=n_params)
+                end = time.time()
                 relay_model = graph_executor.create(relay_graph, lib, dev)
                 relay_model.set_input(**params)
                 for name, inp in compiled_input.items():
@@ -71,10 +89,11 @@ def test_compile(filename, relay_src, print_model=True):
                 relay_model.run()
                 for i, baseline_output in enumerate(baseline_outputs):
                     compiled_output = relay_model.get_output(i).asnumpy()
-                    tvm.testing.assert_allclose(
-                        baseline_output, compiled_output, rtol=1e-5, atol=1e-5
-                    )
+                    print(f'[DEBUG] Building time: {(end - start) * 1000} ms')
+                    print('result:')
                     print(compiled_output)
+                break
 
 if __name__ == '__main__':
-    test_compile('resmlp-dump.json', 'resmlp.relay')
+    import sys
+    test_compile(sys.argv[1], sys.argv[2], depth=int(sys.argv[3]), print_model=int(sys.argv[4]) == 1)
