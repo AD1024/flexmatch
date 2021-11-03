ef066a7cd8785fe96881b9a62e3cba86b5d9ab54
AD1024 <dh63@cs.washington.edu>
2021-11-02 18:07:00 -0700

    [ add ] parameter support for passing rewrite configs

--- flexmatch/src/rewrites.rs
+++ flexmatch/src/rewrites.rs
@@ -2,7 +2,10 @@ use egg::Rewrite;
 use glenside::language::rewrites::*;
 use glenside::language::{Language, MyAnalysis};
 
-pub fn get_rewrite_from_string(name: String) -> Rewrite<Language, MyAnalysis> {
+pub fn get_rewrite_from_string(
+    name: &String,
+    args: &Box<[i32]>,
+) -> Rewrite<Language, MyAnalysis> {
     match name.as_str() {
         "bubble-reshape-through-cartesian-product" => bubble_reshape_through_cartesian_product(),
         "flatten-unflatten-all-accesses" => flatten_unflatten_any_access(),
@@ -11,9 +14,22 @@ pub fn get_rewrite_from_string(name: String) -> Rewrite<Language, MyAnalysis> {
         "bubble-reshape-through-compute-dot-product" => {
             bubble_reshape_through_compute_dot_product()
         }
+        "reassociate-max" => match args[..] {
+            [h, w] => reassociate_max(h as usize, w as usize),
+            _ => panic!("Expecting H and W from reassociate_max, got {:?}", args),
+        },
+        "bubble-access-reshape-through-compute-reduce-max" => {
+            bubble_access_reshape_through_compute_reduce_max()
+        }
+        "simplify-multiple-accesses" => simplify_multiple_accesses(),
+        "simplify-multiple-transposes" => simplify_multiple_transposes(),
+        "simplify-multiple-access-reshapes" => simplify_multiple_access_reshapes(),
+        "bubble-access-through-access-transpose" => bubble_access_through_access_transpose(),
+        "simplify-reduce-max" => simplify_reduce_max(),
 
         "flex-linear-rewrite" => linear_layer_accelerator_rewrites(),
         "vta-dense-rewrite" => dot_product_with_vta(),
+        "flexasr-maxpool" => flexasr_maxpool(),
         _ => {
             eprintln!("{} not implemented", name);
             todo!()
