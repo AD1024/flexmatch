bd720c8dd188d16c4d079d2ab7f8416906b8fcfc
Gus Smith <guscomps@gmail.com>
2021-11-18 20:11:32 -0800

    Need to simplify inference and simplify lets to --debug on transformer

--- tests/run_comparison.py
+++ tests/run_comparison.py
@@ -5,6 +5,7 @@ import time
 from tvm import relay
 from tvm.ir import transform
 from tvm.contrib import graph_executor
+from tvm.relay import ExprMutator
 
 def get_inputs(src):
     with open(src, 'r') as fp:
@@ -20,6 +21,23 @@ def get_inputs(src):
             # inputs.append(np.random.rand(*[int(x) for x in shape]).astype('float32'))
         return inputs
 
+class LetInliner(ExprMutator):
+    def __init__(self):
+        super().__init__()
+        self.let_map = {}
+
+    def visit_var(self, var):
+        if var in self.let_map:
+            return self.let_map[var]
+        return var
+
+    def visit_let(self, let):
+        # don't deal with functions
+        if isinstance(let.value, relay.Function):
+            return super().visit_let(let)
+        self.let_map[let.var] = self.visit(let.value)
+        return self.visit(let.body)
+
 def run_file(src, params):
     print(f'Compiling & Running: {src}')
     with open(src, 'r') as fp:
@@ -27,6 +45,8 @@ def run_file(src, params):
         start = time.time()
         mod = tvm.parser.fromtext(relay_src)
         mod = relay.transform.InferType()(mod)
+        mod = relay.transform.SimplifyInference()(mod)
+        mod = tvm.ir.IRModule.from_expr(LetInliner().visit(mod["main"]))
         inputs = [params[x.name_hint] for x in mod['main'].params]
         for target, dev in tvm.testing.enabled_targets():
             # relay_graph, lib, params = relay.build(mod, target=target, params=params)
