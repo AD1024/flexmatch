89b806a72f73008089498800c6735bd02ecfa54e
AD1024 <dh63@cs.washington.edu>
2023-04-05 13:05:23 -0400

    save data and changes

--- flexmatch/run_with_opt.sh
+++ flexmatch/run_with_opt.sh
@@ -5,11 +5,11 @@ echo "Run EqSat + Extraction with Im2Col + Simplifications"
 for i in {1..5}; do
     for relay_file in ${models[@]}; do
         echo ">>> Extract with WPMAXSAT"
-        timeout 20 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --maxsat
+        timeout 300 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --maxsat
         echo ">>> Extract with ILP-ACyc"
-        timeout 20 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --new-ilp
+        timeout 300 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --new-ilp
         echo ">>> Extract with ILP-Topo"
-        timeout 20 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --topo-sort-ilp
+        timeout 300 ./target/debug/flexmatch ../tests/models/$relay_file rewrite.json data.json simpl.json im2col.json --topo-sort-ilp
     done
 done
 
